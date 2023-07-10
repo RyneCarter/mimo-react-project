811d1b57e56fd46ed647b7053250c7eab37db4db
Ryne Carter <carterryne@gmail.com>
2023-07-10 17:52:39 -0500

    mimo react project1

--- README.md
+++
@@ -1,1 +0,0 @@
-# mimo-react-project
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <h2>Fake Store</h2>
+      <a>Shopping Cart</a>
+
+    </div>
+  )
+}
+
+export default Header
